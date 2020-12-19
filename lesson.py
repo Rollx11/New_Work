8bfecac7980389db00972a4ca8d1eab0b302e299
Rollx11 <76169779+Rollx11@users.noreply.github.com>
2020-12-19 16:19:30 +0300

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,34 @@
+name = input("Введи свое имя:")
+print("привет" " " + name)
+
+
+data = int(input("Укажи дату рождение:"))
+print(data,"число")
+mes = int(input("Месяц рождение:"))
+print(mes,)
+got = int(input("Год рождение:"))
+print(got)
+if (data >=21 and data<=31 and mes==3) or (data>=1 and data<=20 and mes==4):
+    print("твой Знак задиака: Овен" )
+elif (data >=21 and data<=30 and mes==4) or (data>=1 and data<=21 and mes==5):
+    print("твой знак задиака: Телец ")
+if (data >= 20 and data <= 31 and mes == 5) or (data >= 1 and data <= 20 and mes ==6):
+        print("твой Знак задиака: близнецы")
+elif (data >= 21 and data <= 30 and mes == 6) or (data >= 1 and data <= 20 and mes == 7):
+        print("твой знак задиака: рак ")
+if (data >= 20 and data <= 31 and mes == 7) or (data >= 1 and data <= 21 and mes == 8):
+            print("твой Знак задиака: лев")
+elif (data >= 21 and data <= 30 and mes == 8) or (data >= 1 and data <= 20 and mes == 9):
+            print("твой знак задиака: дева ")
+if (data >= 20 and data <= 31 and mes == 9) or (data >= 1 and data <= 21 and mes == 10):
+            print("твой Знак задиака: весы")
+elif (data >= 21 and data <= 30 and mes == 10) or (data >= 1 and data <= 20 and mes == 11):
+            print("твой знак задиака: скарпион ")
+if (data >= 20 and data <= 31 and mes == 9) or (data >= 1 and data <= 21 and mes == 10):
+            print("твой Знак задиака: стрелец")
+elif (data >= 21 and data <= 30 and mes == 10) or (data >= 1 and data <= 20 and mes == 11):
+            print("твой знак задиака: козерог ")
+if (data >= 20 and data <= 31 and mes == 9) or (data >= 1 and data <= 21 and mes == 10):
+            print("твой Знак задиака: водолей")
+elif (data >= 21 and data <= 30 and mes == 10) or (data >= 1 and data <= 20 and mes == 11):
+            print("твой знак задиака: рыбы ")
